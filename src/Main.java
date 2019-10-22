2aec9ff5e855285bbbc462b194e361fda8f64922
Mel <melbelachew@gmail.com>
2019-10-22 13:15:28 -0400

    updated first prompt to user

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args){
 
         Scanner key = new Scanner(System.in);
-        System.out.println("enter 3 numbers");
+        System.out.println("enter 10 numbers");
 
         int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
 
