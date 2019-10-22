5c79fc782b9351ed66a5608effd30176a078834a
Mel <melbelachew@gmail.com>
2019-10-22 12:54:30 -0400

    updated for exercise 3

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,21 @@ public class Main {
         Scanner key = new Scanner(System.in);
         System.out.println("enter 3 numbers");
 
-        int num1, num2, num3;
+        int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
 
         num1 = key.nextInt();
         num2 = key.nextInt();
         num3 = key.nextInt();
-        int sum = num1+num2+num3;
+        num4 = key.nextInt();
+        num5 = key.nextInt();
+        num6 = key.nextInt();
+        num7 = key.nextInt();
+        num8 = key.nextInt();
+        num9 = key.nextInt();
+        num10 =key.nextInt();
 
-        System.out.println("The sum is " + sum);
+        int average = (num1+num2+num3+num4+num5+num6+num7+num8+num9+num10)/10;
+        System.out.println("The average is " + average);
     }
 }
 
